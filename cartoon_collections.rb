a90a99fbf949498a96bd9a36c66e25784cb4ac8a
Stephen Wright <scw248@gmail.com>
2019-01-04 19:43:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,10 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.include?(length > 4)
+  array.select do |calls|
+    calls.length > 4
+    return true
+  end
 end
 
 def find_the_cheese# code an argument here
