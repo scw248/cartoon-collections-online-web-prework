e7c2c24cf8cc9dccdf24f83d1ba135f72023507f
Stephen Wright <scw248@gmail.com>
2019-01-04 19:24:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,9 @@
 def roll_call_dwarves(array)
-  #roll_call = ""
+  roll_call = ""
     array.each.with_index do |dwarf, index|
-    "/#{index}. *#{dwarf}/"
+    roll_call += "/#{index}. *#{dwarf}/" + '\n'
   end
-  #roll_call
+  roll_call
 end
 
 def summon_captain_planet# code an argument here
