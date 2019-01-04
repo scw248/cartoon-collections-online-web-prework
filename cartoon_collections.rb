c96dadb411f4e2a64a90126d7d1b5c46fc6ccec3
Stephen Wright <scw248@gmail.com>
2019-01-04 19:11:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,9 @@
+require "pry"
 def roll_call_dwarves(array)
   roll_call = ""
     array.each.with_index(1) do |dwarf, index|
     roll_call += " #{index}. #{dwarf}"
+    binding.pry
   end
   roll_call
 end
