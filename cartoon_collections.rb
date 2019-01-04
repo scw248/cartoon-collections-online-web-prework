e0a6c5aaa947086dd561b4be273243ec3548fc95
Stephen Wright <scw248@gmail.com>
2019-01-04 19:15:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,22 +1,20 @@
-require "pry"
 def roll_call_dwarves(array)
   roll_call = ""
     array.each.with_index(1) do |dwarf, index|
-    roll_call += " #{index}. #{dwarf}"
-    binding.pry
+    roll_call += "#{index}. #{dwarf}"
   end
   roll_call
 end
 
-# def summon_captain_planet# code an argument here
-#   # Your code here
-# end
+def summon_captain_planet# code an argument here
+  # Your code here
+end
 
-# def long_planeteer_calls# code an argument here
-#   # Your code here
-# end
+def long_planeteer_calls# code an argument here
+  # Your code here
+end
 
-# def find_the_cheese# code an argument here
-#   # the array below is here to help
-#   cheese_types = ["cheddar", "gouda", "camembert"]
-# end
+def find_the_cheese# code an argument here
+  # the array below is here to help
+  cheese_types = ["cheddar", "gouda", "camembert"]
+end
