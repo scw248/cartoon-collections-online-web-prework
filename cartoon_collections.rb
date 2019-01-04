3f02b3376647f2f2f01d85390fd3708794ae86e0
Stephen Wright <scw248@gmail.com>
2019-01-04 19:14:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,15 +9,15 @@ def roll_call_dwarves(array)
   roll_call
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
+# def summon_captain_planet# code an argument here
+#   # Your code here
+# end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+# def long_planeteer_calls# code an argument here
+#   # Your code here
+# end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+# def find_the_cheese# code an argument here
+#   # the array below is here to help
+#   cheese_types = ["cheddar", "gouda", "camembert"]
+# end
